ab636affff46243d90d3a6a2ee0b39826ec45978
JovanAleksicFutureGames <145547495+JovanAleksicFutureGames@users.noreply.github.com>
2025-04-30 15:11:49 +0200

    chages to prefabs and menus

--- Assets/Scripts/InteractibleObjects/MedicalMachine.cs
+++ Assets/Scripts/InteractibleObjects/MedicalMachine.cs
@@ -9,19 +9,27 @@ public class MedicalMachine : MonoBehaviour
     [SerializeField] private float _totalHealCapacity = 30.0f;
     [SerializeField] private AudioClip _healSound;
     [SerializeField] private AudioClip _emptySound;
-    [SerializeField] private HashSet<string> _noHeal = new HashSet<string>();
+    [SerializeField] private AudioClip _healthFull;
+    private HashSet<string> _noHeal = new HashSet<string>();
+    private HashSet<string> _fullHealth = new HashSet<string>();
     private InteractableObject _interactableObject;
     
     private void Awake()
     {
         _interactableObject = GetComponent<InteractableObject>();
         _noHeal.Add("The machine has no energy left.");
+        _fullHealth.Add("You are at full health.");
     }
 
     public void Heal()
     {
         if (GameManager.Instance.PlayerInstance.GetComponent<Stats>().CurrentHealth >=
-            GameManager.Instance.PlayerInstance.GetComponent<Stats>().MaxHealth) return;
+            GameManager.Instance.PlayerInstance.GetComponent<Stats>().MaxHealth)
+        {
+            _interactableObject.SetInteractionPrompt(_fullHealth.First());
+            AudioManager.Instance.PlayEffectDoubleVolume(_healthFull);
+            return;
+        }
         if(_totalHealCapacity <= 0)
         {
             _interactableObject.SetInteractionPrompt(_noHeal.First());
